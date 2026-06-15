20973f4724c2e014c5715953d277f9e4491088bd
Biswajit <pattanaikbiswajit07@gmail.com>
2026-06-15 13:30:43 +0530

    Update route.ts

--- rydex/src/app/api/booking/[id]/arriving/route.ts
+++ rydex/src/app/api/booking/[id]/arriving/route.ts
@@ -4,16 +4,19 @@ import Booking from "@/models/booking.model";
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> } // 1. Fixed: Changed params to a Promise
+  { params }: { params: Promise<{ id: string }> }
 ) {
   await connectDb();
 
-  // 2. Fixed: Await the params to safely get the id
-  const { id } = await params; 
+  const { id } = await params;
 
   const booking = await Booking.findById(id);
+
   if (!booking) {
-    return NextResponse.json({ message: "Not found" }, { status: 404 });
+    return NextResponse.json(
+      { message: "Not found" },
+      { status: 404 }
+    );
   }
 
   booking.status = "arriving";
