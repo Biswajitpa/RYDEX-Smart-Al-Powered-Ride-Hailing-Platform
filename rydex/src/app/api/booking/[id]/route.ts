7d0435fefce6c7be5e81ed9eab038f342b3018f9
Biswajit <pattanaikbiswajit07@gmail.com>
2026-06-18 21:49:54 +0530

    Update route.ts

--- rydex/src/app/api/booking/[id]/route.ts
+++ rydex/src/app/api/booking/[id]/route.ts
@@ -1,32 +1,17 @@
 import connectDb from "@/lib/db";
 import Booking from "@/models/booking.model";
+import axios from "axios";
 import { NextResponse } from "next/server";
-import User from "@/models/user.model"; 
+
 
 export async function GET(
   req: Request,
-  context: { params: Promise<{ id: string }> }
+   context : { params: Promise<{ id: string }> }
 ) {
-  try {
-    await connectDb();
-    const { id } = await context.params;
+  await connectDb();
+ const id=(await context.params).id
+  const booking = await Booking.findById(id).populate("driver vehicle")
 
-    if (!id) {
-      return NextResponse.json({ error: "Missing booking ID" }, { status: 400 });
-    }
 
-    // This fetches the booking and attaches driver details
-    const booking = await Booking.findById(id).populate("driver");
-
-    if (!booking) {
-      return NextResponse.json({ error: "Booking not found" }, { status: 404 });
-    }
-
-    // Sends the object directly to match the frontend
-    return NextResponse.json(booking);
-    
-  } catch (error: any) {
-    console.error("BACKEND FETCH ERROR:", error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+  return NextResponse.json(booking);
 }
